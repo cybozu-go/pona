1b5a60ab3ed7740d15da3b2f07411406c811d2a4
walnuts1018 <r.juglans.1018@gmail.com>
2024-08-21 10:19:34 +0900

    add podspec reconcile logic
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- internal/controller/egress_controller.go
+++ internal/controller/egress_controller.go
@@ -3,12 +3,14 @@ package controller
 import (
 	"context"
 
+	ponav1beta1 "github.com/cybozu-go/pona/api/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	ponav1beta1 "github.com/cybozu-go/pona/api/v1beta1"
 )
 
 // EgressReconciler reconciles a Egress object
@@ -21,6 +23,10 @@ type EgressReconciler struct {
 // +kubebuilder:rbac:groups=pona.cybozu.com,resources=egresses/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=pona.cybozu.com,resources=egresses/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;update;patch
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -31,16 +37,77 @@ type EgressReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
 func (r *EgressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var egress ponav1beta1.Egress
+	err := r.Get(ctx, req.NamespacedName, &egress)
+	if errors.IsNotFound(err) {
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		logger.Error(err, "unable to get Egress",
+			"name", req.Name,
+			"namespace", req.Namespace,
+		)
+		return ctrl.Result{}, err
+	}
+
+	if !egress.ObjectMeta.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
+
+	if err = r.reconcileDeployment(ctx, egress); err != nil {
+		result, err2 := r.updateStatus(ctx, egress)
+		logger.Error(err2, "unable to update status")
+		return result, err
+	}
+
+	if err = r.reconcileService(ctx, egress); err != nil {
+		result, err2 := r.updateStatus(ctx, egress)
+		logger.Error(err2, "unable to update status")
+		return result, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *EgressReconciler) reconcileDeployment(ctx context.Context, egress ponav1beta1.Egress) error {
+	logger := log.FromContext(ctx)
+
+	dep := &appsv1.Deployment{}
+	dep.SetNamespace(egress.Namespace)
+	dep.SetName(egress.Name)
+
+	return nil
+}
+
+func (r *EgressReconciler) reconcilePodTemplate(egress *ponav1beta1.Egress, deploy *appsv1.Deployment) error {
+	target := &deploy.Spec.Template
+	target.Labels = make(map[string]string)
+	if target.Annotations == nil {
+		target.Annotations = make(map[string]string)
+	}
+
+	desired := egress.Spec.Template
+	podSpec := &corev1.PodSpec{}
+	if desired != nil {
+		podSpec = desired.Spec.DeepCopy()
+		for k, v := range desired.Annotations {
+			target.Annotations[k] = v
+		}
+		for k, v := range desired.Labels {
+			target.Labels[k] = v
+		}
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *EgressReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ponav1beta1.Egress{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
