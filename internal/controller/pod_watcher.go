e0e39fb03779a2de2c7df5d21416b40840fee5a1
gotti <18141824+gotti@users.noreply.github.com>
2024-08-23 11:52:16 +0900

    [WIP] implementing pod watcher
    Signed-off-by: gotti <18141824+gotti@users.noreply.github.com>

--- internal/controller/pod_watcher.go
+++ internal/controller/pod_watcher.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"strings"
+	"sync"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -24,6 +26,8 @@ type PodReconciler struct {
 
 	EgressName      string
 	EgressNamespace string
+
+	linkMutex sync.Mutex
 }
 
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
@@ -45,13 +49,32 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		if apierrors.IsNotFound(err) {
 			if err := r.handlePodDeletion(ctx, req.NamespacedName); err != nil {
 				logger.Error(err, "failed to remove tunnel")
-				return ctrl.Result{}, err
+				return ctrl.Result{}, fmt.Errorf("failed to remove tunnel: %w", err)
 			}
 			return ctrl.Result{}, nil
 		}
+		return ctrl.Result{}, fmt.Errorf("failed to get Pod: %w", err)
+	}
+
+	// Pod is terminated or terminating
+	if isTerminated(pod) || pod.DeletionTimestamp != nil {
+		if err := r.handlePodDeletion(ctx, req.NamespacedName); err != nil {
+			logger.Error(err, "failed to remove tunnel for terminated pod")
+			return ctrl.Result{}, fmt.Errorf("failed to remove tunnel for terminated pod: %w", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	if err := r.handlePodRunning(ctx, pod); err != nil {
+		logger.Error(err, "failed to setup tunnel")
+		return ctrl.Result{}, fmt.Errorf("failed to setup tunnel: %w", err)
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func isTerminated(pod *corev1.Pod) bool {
+	return pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed
 }
 
 func (r *PodReconciler) shouldHandle(pod *corev1.Pod) bool {
@@ -63,11 +86,21 @@ func (r *PodReconciler) shouldHandle(pod *corev1.Pod) bool {
 	return r.hasEgressAnnotation(pod)
 }
 
+func (r *PodReconciler) handlePodRunning(ctx context.Context, pod *corev1.Pod) error {
+	logger := log.FromContext(ctx)
+
+	r.linkMutex.Lock()
+	defer r.linkMutex.Unlock()
+	return fmt.Errorf("is not implemented")
+}
+
 // TODO
 func (r *PodReconciler) handlePodDeletion(ctx context.Context, namespacedName types.NamespacedName) error {
 	logger := log.FromContext(ctx)
 
-	return nil
+	r.linkMutex.Lock()
+	defer r.linkMutex.Unlock()
+	return fmt.Errorf("is not implemented")
 }
 
 func (r *PodReconciler) hasEgressAnnotation(pod *corev1.Pod) bool {
