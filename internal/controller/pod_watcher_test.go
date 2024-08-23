46ee0c066c394325ecf5a4ecc8cfe4983101b9ac
walnuts1018 <r.juglans.1018@gmail.com>
2024-08-23 17:35:01 +0900

    Implement pod_watcher_test
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- internal/controller/pod_watcher_test.go
+++ internal/controller/pod_watcher_test.go
@@ -1,16 +1,174 @@
 package controller
 
 import (
+	"context"
+	"net/netip"
+	"path/filepath"
+
+	"github.com/cybozu-go/pona/internal/tunnel"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var _ = Describe("Pod Controller", func() {
+type mockTunnel struct {
+	tunnels map[netip.Addr]struct{}
+}
+
+var _ tunnel.Tunnel = &mockTunnel{}
+
+func NewMockTunnel() mockTunnel {
+	return mockTunnel{
+		tunnels: make(map[netip.Addr]struct{}),
+	}
+}
+
+func (m mockTunnel) Add(addr netip.Addr) error {
+	m.tunnels[addr] = struct{}{}
+	return nil
+}
+
+func (m mockTunnel) Del(addr netip.Addr) error {
+	delete(m.tunnels, addr)
+	return nil
+}
+
+func (m mockTunnel) Init() error {
+	return nil
+}
+
+func (m mockTunnel) IsInitialized() bool {
+	return true
+}
+
+type pod struct {
+	NamespacedName types.NamespacedName
+	PodIPs         []netip.Addr
+}
+
+var _ = Describe("Pod Watcher", func() {
 	Context("When reconciling a resource", func() {
+		ctx := context.Background()
+		const (
+			egressName      = "egress"
+			egressNamespace = "default"
+		)
+
+		podInfo := pod{
+			NamespacedName: types.NamespacedName{
+				Name:      "pod",
+				Namespace: "default",
+			},
+			PodIPs: []netip.Addr{netip.MustParseAddr("192.168.0.1")},
+		}
+		pod := &corev1.Pod{}
+
+		BeforeEach(func() {
+			pod.SetName(podInfo.NamespacedName.Name)
+			pod.SetNamespace(podInfo.NamespacedName.Namespace)
+			pod.Spec.Containers = []corev1.Container{
+				{
+					Name:  "container",
+					Image: "image",
+				},
+			}
+
+			pod.Annotations = map[string]string{
+				filepath.Join(EgressAnnotationPrefix, egressNamespace): egressName,
+			}
+
+			By("create pod")
+			err := k8sClient.Create(ctx, pod)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("set pod status")
+			Eventually(func() error {
+				return k8sClient.Get(ctx, client.ObjectKey(podInfo.NamespacedName), pod)
+			}).Should(Succeed())
+
+			pod.Status.PodIPs = make([]corev1.PodIP, 0, len(podInfo.PodIPs))
+			for _, ip := range podInfo.PodIPs {
+				pod.Status.PodIPs = append(pod.Status.PodIPs, corev1.PodIP{IP: ip.String()})
+			}
+
+			err = k8sClient.Status().Update(ctx, pod)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+		})
 
 		It("should successfully reconcile the resource", func() {
+			By("Reconcile the created resource")
+			t := NewMockTunnel()
+			w := &PodWatcher{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+				tun:    t,
 
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+				EgressName:      egressName,
+				EgressNamespace: egressNamespace,
+
+				podToPodIPs: make(map[types.NamespacedName][]netip.Addr),
+				podIPToPod:  make(map[netip.Addr]Set[types.NamespacedName]),
+			}
+
+			_, err := w.Reconcile(ctx, reconcile.Request{
+				NamespacedName: podInfo.NamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Check if Add() is called")
+			for _, ip := range podInfo.PodIPs {
+				_, ok := t.tunnels[ip]
+				Expect(ok).To(BeTrue())
+			}
+
+			By("Check podToPodIPs, podIPsToPod")
+			Expect(w.podToPodIPs).To(Equal(map[types.NamespacedName][]netip.Addr{
+				podInfo.NamespacedName: podInfo.PodIPs,
+			}))
+			Expect(w.podIPToPod).To(Equal(podIPToPod(podInfo)))
+
+			By("Delete Pod")
+
+			err = k8sClient.Delete(ctx, pod)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Reconcile the created resource")
+			_, err = w.Reconcile(ctx, reconcile.Request{
+				NamespacedName: podInfo.NamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Check if Delete() is called")
+			for _, ip := range podInfo.PodIPs {
+				_, ok := t.tunnels[ip]
+				Expect(ok).To(BeFalse())
+			}
+
+			By("Check podToPodIPs, podIPsToPod")
+			Expect(w.podToPodIPs).To(Equal(map[types.NamespacedName][]netip.Addr{}))
+			Expect(w.podIPToPod).To(Equal(map[netip.Addr]Set[types.NamespacedName]{}))
+
 		})
 	})
 })
+
+func podIPToPod(pod pod) map[netip.Addr]Set[types.NamespacedName] {
+	m := make(map[netip.Addr]Set[types.NamespacedName])
+	for _, v := range pod.PodIPs {
+		nns, ok := m[v]
+		if !ok {
+			m[v] = Set[types.NamespacedName]{
+				pod.NamespacedName: struct{}{},
+			}
+		} else {
+			nns[pod.NamespacedName] = struct{}{}
+		}
+	}
+	return m
+}
