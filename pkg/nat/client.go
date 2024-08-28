e865a5c036342d273806f4e7a29a0ad925f78456
walnuts1018 <r.juglans.1018@gmail.com>
2024-08-28 15:10:38 +0900

    [WIP] UpdateRoutes
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- pkg/nat/client.go
+++ pkg/nat/client.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"maps"
 	"net/netip"
-	"slices"
 	"sync"
 
 	"github.com/cybozu-go/pona/pkg/util/netiputil"
 	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 )
 
 // IDs
@@ -55,18 +55,23 @@ type natClient struct {
 	ipv4 bool
 	ipv6 bool
 
-	v4priv []*netip.Prefix
-	v6priv []*netip.Prefix
+	v4priv []netip.Prefix
+	v6priv []netip.Prefix
 
 	mu sync.Mutex
 }
 
-func NewNatClient(ipv4, ipv6 netip.Addr, podNodeNet []netip.Prefix) (Client, error) {
-	if !ipv4.Is4() {
+func NewNatClient(ipv4, ipv6 *netip.Addr, podNodeNet []netip.Prefix) (Client, error) {
+	if ipv4 != nil && !ipv4.Is4() {
 		return nil, errors.New("invalid ipv4 address")
 	}
-	if !ipv6.Is6() {
+
+	if ipv6 != nil && !ipv6.Is6() {
 		return nil, errors.New("invalid ipv6 address")
+	}
+
+	if ipv4 == nil && ipv6 == nil {
+		return nil, errors.New("invalid arguments")
 	}
 
 	var v4priv, v6priv []netip.Prefix
@@ -194,23 +199,46 @@ func (c *natClient) IsInitialized() (bool, error) {
 }
 
 func (c *natClient) UpdateRoutes(link netlink.Link, subnets []netip.Prefix) error {
-	routes, err := collectRoutes(link.Attrs().Index)
+	current, err := collectRoutes(link.Attrs().Index)
 	if err != nil {
 		return fmt.Errorf("failed to collect routes: %w", err)
 	}
 
 	var adds []netip.Prefix
-	var dels []netip.Prefix
+	var dels []netlink.Route
 
 	for _, n := range subnets {
-		if _, ok := routes[n]; !ok {
+		if _, ok := current[n]; !ok {
 			adds = append(adds, n)
 		}
 	}
 
-	slices.DeleteFunc(routes, func(p netip.Prefix) bool {
+	subnetsSet := subnetsSet(subnets)
+	for subnet, route := range current {
+		if _, ok := subnetsSet[subnet]; !ok {
+			dels = append(dels, route)
+		}
+	}
 
-	})
+	// link up here to minimize the down time
+	// See https://github.com/cybozu-go/coil/issues/287.
+	if err := netlink.LinkSetUp(link); err != nil {
+		return fmt.Errorf("netlink: failed to link up %s: %w", link.Attrs().Name, err)
+	}
+
+	for _, v := range v4PrivateList {
+		c.addThrow(link, v)
+	}
+	for _, v := range v6PrivateList {
+		c.addThrow(link, v)
+	}
+
+	for _, n := range adds {
+		if err := c.addRoute(link, n); err != nil {
+			return err
+		}
+	}
+
 }
 
 func collectRoutes(linkIndex int) (map[netip.Prefix]netlink.Route, error) {
@@ -243,4 +271,71 @@ func collectRoute1(linkIndex, family int) (map[netip.Prefix]netlink.Route, error
 		}
 	}
 	return routes, nil
+}
+
+func subnetsSet(subnets []netip.Prefix) map[netip.Prefix]struct{} {
+	subnetsSet := make(map[netip.Prefix]struct{})
+	for _, subnet := range subnets {
+		subnetsSet[subnet] = struct{}{}
+	}
+	return subnetsSet
+}
+
+func (c *natClient) addThrow(link netlink.Link, n netip.Prefix) error {
+	dst := netiputil.ToIPNet(n)
+	err := netlink.RouteAdd(&netlink.Route{
+		Table:     ncTableID,
+		Dst:       &dst,
+		Type:      unix.RTN_THROW,
+		LinkIndex: link.Attrs().Index,
+		Protocol:  ncProtocolID,
+	})
+	if err != nil {
+		return fmt.Errorf("netlink: failed to add route(table %d) to %s: %w", ncTableID, n.String(), err)
+	}
+
+	return nil
+}
+
+func (c *natClient) addRoute(link netlink.Link, n netip.Prefix) error {
+	var priv []netip.Prefix
+	if n.Addr().Is4() {
+		if !c.ipv4 {
+			return nil
+		}
+		priv = c.v4priv
+	} else {
+		if !c.ipv6 {
+			return nil
+		}
+		priv = c.v6priv
+	}
+
+	for _, p := range priv {
+		if !p.Contains(n.IP) {
+			continue
+		}
+
+		err := netlink.RouteAdd(&netlink.Route{
+			Table:     ncNarrowTableID,
+			Dst:       n,
+			LinkIndex: link.Attrs().Index,
+			Protocol:  ncProtocolID,
+		})
+		if err != nil {
+			return fmt.Errorf("netlink: failed to add route(table %d) to %s: %w", ncNarrowTableID, n.String(), err)
+		}
+		return nil
+	}
+
+	err := netlink.RouteAdd(&netlink.Route{
+		Table:     ncWideTableID,
+		Dst:       n,
+		LinkIndex: link.Attrs().Index,
+		Protocol:  ncProtocolID,
+	})
+	if err != nil {
+		return fmt.Errorf("netlink: failed to add route(table %d) to %s: %w", ncWideTableID, n.String(), err)
+	}
+	return nil
 }
