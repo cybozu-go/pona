8f59337671c603c43233c86e06160cdd90f9c16b
walnuts1018 <r.juglans.1018@gmail.com>
2024-08-29 09:46:41 +0900

    implement pona cmdAdd
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- cmd/pona/rpc.go
+++ cmd/pona/rpc.go
@@ -12,6 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/status"
 )
 
 // PluginEnvArgs represents CNI_ARG
@@ -58,4 +59,20 @@ func connect(sockPath string) (*grpc.ClientConn, error) {
 		return nil, fmt.Errorf("failed to connect to %s: %w", sockPath, err)
 	}
 	return conn, nil
+}
+
+// convertError turns err returned from gRPC library into CNI's types.Error
+func convertError(err error) error {
+	st := status.Convert(err)
+	details := st.Details()
+	if len(details) != 1 {
+		return types.NewError(types.ErrInternal, st.Message(), err.Error())
+	}
+
+	cniErr, ok := details[0].(*cnirpc.CNIError)
+	if !ok {
+		types.NewError(types.ErrInternal, st.Message(), err.Error())
+	}
+
+	return types.NewError(uint(cniErr.Code), cniErr.Msg, cniErr.Details)
 }
